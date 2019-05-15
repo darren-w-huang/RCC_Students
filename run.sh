1a8568b116589042cd7570b7bc03fdf9e7c797f6
Michelle Yang <miyang@cs.washington.edu>
2019-05-15 13:15:10 -0700

    cleaned up files, added script, updated README.md

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Michelle Yang copyright 2019
+# script that generates necessary files
+
+SCRIPT=data_analysis/script_data.json
+
+if [ ! 1 -eq $# ]; then
+   echo "Usage: ./run <*.json>"
+   exit 1
+fi
+
+if [ ! -r $1 ]; then
+    echo "File does not exist or read permission not granted."
+    exit 1
+fi
+
+echo "info = " > $SCRIPT
+cat $1 >> $SCRIPT
